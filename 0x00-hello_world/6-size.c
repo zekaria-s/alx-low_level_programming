ce0c7d05b9c726a21ac99dc1ff48fa0d3567a57e
zekaria-s <zekariasdemis@gmail.com>
2023-02-16 05:31:59 -0800

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
-* main - a program that prints the size of various computer types
-* Return 0 (success)
-*/
+ * main - a program that prints the size of various computer types
+ * Return 0 (success)
+ */
 int main(void)
 {
 char a;
