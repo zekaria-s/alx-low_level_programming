54a3e6785bc6cf730d3d978004ca3215092ab62f
zekaria-s <zekariasdemis@gmail.com>
2023-02-16 05:24:20 -0800

    size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+* main - a program that prints the size of various computer types
+*Return 0(success)
+*/
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
+return (0);
+}
